68322bd1c588036d60bdec12b9d2ff807c8edd30
Igor Machado <igorhms98@gmail.com>
2020-05-13 22:17:14 -0300

    Leitura de dados1

--- src/application/simula.java
+++ src/application/simula.java
@@ -1,12 +1,26 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class simula {
 
 	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int resposta;
+
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("Ensino Fundamental");
+		System.out.println("Ensino Médio");
+		System.out.println("Ensino Superior");
+		System.out.println("Pós-graduação");
+		System.out.print("Digite uma opção: ");
+		resposta = sc.nextInt();
 		
-		System.out.println("Hello World");
-		
-		
+		sc.close();
 	}
 
 }
