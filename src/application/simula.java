43e72338fb077bef373226a89a5e8ad25eaee6e4
Igor Machado <igorhms98@gmail.com>
2020-05-13 22:19:46 -0300

    Escolaridade e experiência

--- src/application/simula.java
+++ src/application/simula.java
@@ -30,6 +30,35 @@ public class simula {
 		habilitacao = sc.next();
 		System.out.println();
 		
+		if (resposta == 1){
+			pontoEscolaridade = 10;
+		}
+		else if (resposta == 2) {
+				pontoEscolaridade = 20;
+		}
+		else if (resposta == 3){
+				pontoEscolaridade = 30;
+		}
+		else {
+			pontoEscolaridade = 40;
+		}
+		System.out.printf("Pontos por escolaridade: %d%n", pontoEscolaridade);
+		
+		if (experiencia == 0){
+			pontoExperiencia = 0;
+		}
+		else if (experiencia < 2){
+			pontoExperiencia = 10;
+		}
+		else if (experiencia > 2 && experiencia < 5){
+			pontoExperiencia = 20;
+		}
+		else {
+			pontoExperiencia = 40;
+		}
+		System.out.printf("Pontos por experiência: %d%n", pontoExperiencia);
+		System.out.println();
+		
 		sc.close();
 	}
 
