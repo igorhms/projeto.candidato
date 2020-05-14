a4e91efd1041ead0d9c11d248c7b485fc1edeb2c
Igor Machado <igorhms98@gmail.com>
2020-05-13 22:14:33 -0300

    Leitura da escolaridade

--- src/application/simula.java
+++ src/application/simula.java
@@ -3,8 +3,10 @@ package application;
 public class simula {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hello World");
+		
+		
 	}
 
 }
